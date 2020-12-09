cf015d93acedf45e47aa416fa04b6055a934f3d3
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-12-09 01:51:34 -0500

    add activity feature

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     //the bricks for the wall
-    const characterArrayPlainText = [
+    const activityArray = [
         "get drunk", "eat delicious food", "play some intense vidya", "work out", "go to sleep"
     ];
 
-    //selects a random character
-    function getRandomChar(max) {
+    //selects a random activity
+    function getRandomInt(max) {
         return (Math.floor(Math.random() * Math.floor(max)));
     }
 
     function createNewActivity() {
-        return characterArrayPlainText[getRandomChar(characterArrayPlainText.length)];
+        return activityArray[getRandomInt(activityArray.length)];
     }
 
-    //respond to make new-password user request
-    const buttonElement = document.getElementById('receive-activity');
-    buttonElement.addEventListener("click", function () {
+    //create a new activity in activityArray
+    const createButtonElement = document.getElementById('create-activity');
 
+    createButtonElement.addEventListener("click", function () {
+
+        const newInputElement = document.getElementById('input-activity');
+        if (newInputElement.value != "") {
+            activityArray.push(newInputElement.value);
+        }
+
+    });
+
+    //respond to make new-activity user request
+    const printButtonElement = document.getElementById('receive-activity');
+
+    printButtonElement.addEventListener("click", function () {
+
+        const newPrintElement = document.getElementById('new-activity');
+        newPrintElement.innerHTML = `${createNewActivity()}`;
       
-
-        const newPasswordElement = document.getElementById('new-activity');
-        newPasswordElement.innerHTML += `
-            <p>${createNewActivity()}</p>
-            <hr>
-        `;
     });
 });
