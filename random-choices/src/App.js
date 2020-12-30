7732ac648abecbec05dc6de9afa1ed60c9d1740b
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-12-30 05:07:21 -0500

    converting the html and css to reactjs

--- random-choices/src/App.js
+++ random-choices/src/App.js
@@ -4,18 +4,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+        <body className="App-body">
+            <fieldset className="App-fieldset">
+                <div>Add a new activity: </div>
+                <input id="input-activity" placeholder="type an activity" value="" />
+                <button id="create-activity">Create a new activity</button>
+            </fieldset>
+                <br />
+            <fieldset className="App-fieldset">
+                <div>Press button for a random activity to do: </div>
+                <button id="receive-activity">Receive a new activity</button>
+            </fieldset>
+                <br />
+            <fieldset className="App-fieldset">
+                <p id="new-activity" >
+                    ???????
+                </p>
+            </fieldset>
+                <br />
+            <fieldset className="App-fieldset">
+                <p id="i-was-here">Created by <a href="https://github.com/cireneirbo">Eric O&apos;Brien</a></p>
+            </fieldset>
+                <br />
+                <br />
+        </body>
+        <footer className="App-footer">
+        </footer>
     </div>
   );
 }
